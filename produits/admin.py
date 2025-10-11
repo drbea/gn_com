8c3331f6fe124cc27d9115b7e50076889c075ee3
drbea <beavoguimaximeakoi@gmail.com>
2025-10-11 18:00:24 +0000

    gestion product admin

--- produits/admin.py
+++ produits/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Categorie, Images, Produit, Article
+
+
+class CategorieAdmin(admin.ModelAdmin):
+ list_display = ("nom", "description")
+
+class ImagesInline(admin.TabularInline):
+    model = Comment
+    extra = 1 # Show one empty comment form by default
+
+
+
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ("nom", "description", "categorie")
+    inlines = [CommentInline]
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("nom", "prix", "date_added", "marque", "couleur",)
+    inlines = [CommentInline]
--- produits/models.py
+++ produits/models.py
@@ -9,12 +9,43 @@ produit:
     nom, description, images, date_added, Categorie
 
 Article:
-   nom, decoration, prix, images, date_added, marque, couleur,
+   nom, description, prix, images, date_added, marque, couleur,
 
 """
 class Categorie(models.Model):
     nom = models.CharField(max_length = 100, unique = True)
     description = models.TextField(blank = true)
+
+    def __str__(self):
+        return f"{self.nom}"
+
+class Images(models.Model):
+    nom = models.CharField(max_length = 100, blank=True)
+    def __str__(self):
+        return f"{self.nom}"
+
+
+
+class Produit(models.Model):
+    nom = models.CharField(max_length = 100, unique = True)
+    description = models.TextField(blank = true)
+    images = models.ForeignKey(Images, on_delete = models.CASCADE, blank = True)
+    date_added = models.DateTimeField(aut_now = True)
+    categorie =  models.ForeignKey(Categorie, on_delete = models.CASCADE, blank = True)
+
+    def __str__(self):
+        return f"{self.nom}"
+
+
+class Article(models.Model):
+    nom = models.CharField(max_length = 100, unique = True)
+    description = models.TextField(blank = true)
+    images = models.ForeignKey(Images, on_delete = models.CASCADE, blank = True)
+    date_added = models.DateTimeField(aut_now = True)
+    categorie =  models.ForeignKey(Categorie, on_delete = models.CASCADE, blank = True)
+    marque =  models.CharField(max_length = 100, blank = True)
+    couleur = models.CharField(max_length = 100, blank = True)
+    prix = models.DecimalField(max_digits = 10, decimal_places = 2)
 
     def __str__(self):
         return f"{self.nom}"
